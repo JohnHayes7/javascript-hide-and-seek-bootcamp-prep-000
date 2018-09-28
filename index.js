52973edf18d802cc51254fae5ba796c3fff0cc6b
John Hayes <johnnyhayes7@gmail.com>
2018-09-28 20:19:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector){
+ var firstSelector = document.querySelector (selector);
+ return firstSelector;
+}
 
+function nestedTarget(){
+  var nest=document.getElementById('nested').querySelector ('.target');
+  return nest;
+}
+
+function increaseRankBy(n){
+  const ranked = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i=0; i<ranked.length; i++){
+    ranked[i].innerHTML=parseInt(ranked[i].innerHTML) + n;
+  }
+  return ranked;
+}
+
+function deepestChild(){
+ const deep=document.getElementById('grand-node').querySelectorAll('div');
+ for (var i=0; i<deep.length-1; i++){
+ var response=deep[i].querySelector('div');
+  }
+  return response;
+}
