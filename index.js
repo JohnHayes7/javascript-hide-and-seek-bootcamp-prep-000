8ffeb17ed399b9a2c80cf8438e171bddf36ad020
John Hayes <johnnyhayes7@gmail.com>
2018-10-01 15:33:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 function getFirstSelector(selector){
- var firstSelector = document.querySelector (selector);
- return firstSelector;
+  var result=document.querySelector(selector);
+  return result;
 }
 
 function nestedTarget(){
-  var nest=document.getElementById('nested').querySelector ('.target');
-  return nest;
+ const nest=document.querySelector('#nested .target');
+ return nest;
 }
 
 function increaseRankBy(n){
-  const ranked = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-  for (var i=0; i<ranked.length; i++){
-    ranked[i].innerHTML=parseInt(ranked[i].innerHTML) + n;
-  }
-  return ranked;
-}
-
-function deepestChild(){
- const deep=document.getElementById('grand-node').querySelectorAll('div');
- for (var i=0; i<deep.length-1; i++){
- var response=deep[i].querySelector('div');
-  }
-  return response;
+  var lists=document.getElementByClass('ranked-list')
 }
