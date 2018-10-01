437a9228088f1f0eb882049ee46e4f56e7e5706a
John Hayes <johnnyhayes7@gmail.com>
2018-10-01 15:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   var lists=document.querySelectorAll('.ranked-list');
   for (let i=0; i<lists.length; i++){
-    lists=lists.innerHTML+n;
+    lists=lists.innerHTML+parseInt(lists+n);
   }
   return lists;
 }
